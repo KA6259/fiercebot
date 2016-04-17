f277398ade993abe604551e0cb0ad660a8ab4a8d
KA6259 <karanarora.in@gmail.com>
2016-04-16 23:45:27 -0700

    Adding message handler

--- index.js
+++ index.js
@@ -18,13 +18,29 @@ app.get('/', function(request, response) {
 
 
 // this is for fb messenger
-app.get('/webhook', function (req, res) {
+app.get('/webhook/', function (req, res) {
   console.log('WEBHOOK works')
   if (req.query['hub.verify_token'] === 'LUKzfBrS') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 })
+
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
+
 
 app.get('/cool', function(request, response) {
   response.send(cool());
