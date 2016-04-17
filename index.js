6fc5495324324698a897767bbbaefbd3b91b588c
KA6259 <karanarora.in@gmail.com>
2016-04-17 02:27:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook/', function (req, res) {
   console.log('Webhook post works');
   console.log('Request Body:')
-  console.log(req.body);
-  messaging_events = req.body.entry[0].messaging;
+  console.log(req.json);
+  messaging_events = req.json.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
+    event = req.json.entry[0].messaging[i];
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
