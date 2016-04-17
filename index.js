36fa05b780e5ce67cbe59303430b6b642a181d62
KA6259 <karanarora.in@gmail.com>
2016-04-17 01:00:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/webhook', function (req, res) {
 // handle messages from FB Messenger
 app.post('/webhook/', function (req, res) {
   console.log('Webhook post works');
-  messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
