42682fd27094048d75370577292684ec5910b910
KA6259 <karanarora.in@gmail.com>
2016-04-17 00:18:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ app.get('/', function(request, response) {
 });
 
 
+var token = "CAADVqZBlMB7kBALCWQadDghHx3VoZBZCZBNcwNoDk5cF8Or70nVYrJ1mK2JfZCcGnuclgBdQcrDKdKbK2GNDU2VmOspiqXzXQsl2THZCZAn8Im9x8zFU4BFcOG8H8LpWNZAhpr3TknZCzs3Teh9oeIL8M562IhNZCFLjZAd97Q8S2DkcDFOV4DhoHKfxRdrrE8f7TUZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 // this is for fb messenger
 app.get('/webhook', function (req, res) {
   console.log('WEBHOOK works')
@@ -35,7 +58,7 @@ app.post('/webhook', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      // Handle a text message from this sender
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
       console.log('Received a message')
     }
   }
