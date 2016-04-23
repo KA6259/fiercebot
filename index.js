38b5ba02c4317cd9637bc294be7df7abe51b8d88
KA6259 <karanarora.in@gmail.com>
2016-04-23 01:12:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var messaging_events;
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
