4de94c19a66fd45452a4e49a39c40985e2676832
KA6259 <karanarora.in@gmail.com>
2016-04-23 02:00:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require("request")
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
